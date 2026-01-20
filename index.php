634e0de04f2f0b2be87147728490e1d51013e3be
puviyarasimanian <puvi0307@gmail.com>
2026-01-20 14:41:48 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$nama = "PUVIYARASI MANIAN";
+$nomatrik = "18DIT24F1029";
+$kelas = "DIT4D";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Profile</title>
+</head>
+<body>
+	<h2>My Profile</h2>
+	<p>Name: <?= $nama ?></p>
+	<p>Matric No: <?= $nomatrik ?></p>
+	<p>Class: <?= $kelas ?></p>
+	<img src="puvi.jpg" width="200">
+</body>
+</html>
